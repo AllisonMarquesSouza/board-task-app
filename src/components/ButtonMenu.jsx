e98f9594ee4fe90c69ee8a2bee1aba20436f35e2
Allison <allisonsouza10261@gmail.com>
2026-08-17 19:10:06 -0300

    Add options when button menu is activeted

--- src/components/ButtonMenu.jsx
+++ src/components/ButtonMenu.jsx
@@ -1,9 +1,35 @@
-import { Menu } from "lucide-react";
+import { CodeXml, Menu, House } from "lucide-react";
+import { useState } from "react";
 
 export default function ButtonMenu() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <button className="bg-slate-900 text-white p-2 rounded-full">
-      <Menu></Menu>
-    </button>
+    <div className="flex flex-col">
+      <button
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+        className=" text-[#F3F4F4] hover:bg-[#F3F4F4]/50 transition p-2 rounded-full"
+      >
+        <Menu></Menu>
+      </button>
+      {isMenuOpen && (
+        <div className="flex flex-col gap-2 text-white ">
+          <a
+            href="https://github.com/AllisonMarquesSouza/board-task-app"
+            target="_blank"
+            rel="noreferrer"
+            className="flex gap-2 bg-[#F3F4F4] text-[#2C2C2C] hover:bg-[#F3F4F4]/50 transition p-2 rounded-md"
+          >
+            <CodeXml />
+            <span> Source Code</span>
+          </a>
+
+          <button className=" flex gap-2 bg-[#F3F4F4] text-[#2C2C2C] p-2 hover:bg-[#F3F4F4]/50 transition rounded-md">
+            <House />
+            <span> Main Page</span>
+          </button>
+        </div>
+      )}
+    </div>
   );
 }
