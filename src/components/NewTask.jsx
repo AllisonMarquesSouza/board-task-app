3501a22862a1b10bc7bcfd8c3dae242d3263e3ca
Allison <allisonsouza10261@gmail.com>
2026-08-17 19:08:47 -0300

    Adding board list in NewTask to keep board options updated

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
-import TaskFormButtons from "./TaskFormButtons";
+import FormButton from "./FormButton";
+import InputText from "./InputText";
 
-export default function NewTask({ cancelNewTask, onAddNewTask }) {
+export default function NewTask({
+  cancelNewTask,
+  onAddNewTask,
+  board,
+  boards,
+}) {
   /*
 fixed means: Position this element relative to the browser viewport. Basically won't be inside the board anymore, but inside the whole page itself.
 
@@ -13,7 +19,12 @@ So the outer <div> covers the entire scree
 
 */
 
-  const [column, setColumn] = useState("todo");
+  const [optionBoardName, setOptionBoardName] = useState(board.name);
+  /*
+   * Defined the optionBoardName by default the board.name, it means that the board you click in,
+   * will also be selected to add a new task. Which makes total sense
+   * REMEMBER -> OptionBoardName === board.name (I kept the same pattern, to make things easier.)
+   */
   const [title, setTitle] = useState(null);
 
   function handleNewTask(title, column) {
@@ -24,37 +35,41 @@ So the outer <div> covers the entire scree
   }
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black/50">
-      <div className="flex flex-col gap-8 bg-slate-900 p-5 size-96 rounded-lg">
-        <h1 className="text-center text-xl font-bold text-slate-50">
+    <div className="fixed inset-0 flex items-center justify-center bg-[#2C2C2C]/50">
+      <div className="flex flex-col gap-8 bg-[#2C2C2C] p-5 size-96 rounded-lg">
+        <h1 className="text-center text-xl font-bold text-[#F3F4F4]">
           New Task
         </h1>
 
-        {/* another component.. */}
-        <div className="flex flex-col gap-4">
-          <input
+        <div className="flex flex-col gap-4 ">
+          <InputText
+            placeholder={"Enter title of the task"}
             onChange={(event) => setTitle(event.target.value)}
-            type="text"
-            placeholder="Enter title of the task"
-            className="p-2 rounded-lg"
           />
           <select
-            className="p-2 rounded-md text-slate-900"
-            onChange={(event) => setColumn(event.target.value)}
+            className="p-2 rounded-md text-[#2C2C2C]"
+            value={optionBoardName}
+            onChange={(event) => setOptionBoardName(event.target.value)}
           >
-            <option value="todo">To Do</option>
-            <option value="doing">Doing</option>
-            <option value="done">Done</option>
-            <option value="testing">Testing</option>
+            {boards.map((boardGet) => {
+              return (
+                //I need to return the value, this is a function, so the function of component will only get the value
+                // if it is returned
+                <option key={boardGet.id} value={boardGet.name}>
+                  {boardGet.name}
+                </option>
+              );
+            })}
           </select>
         </div>
 
-        {/* another component... */}
         <div className="flex gap-2">
-          <TaskFormButtons onClick={() => handleNewTask(title, column)}>
+          <FormButton
+            onClick={() => handleNewTask(title, optionBoardName)} //why is optionBoard null?
+          >
             Save
-          </TaskFormButtons>
-          <TaskFormButtons onClick={cancelNewTask}>Cancel</TaskFormButtons>
+          </FormButton>
+          <FormButton onClick={cancelNewTask}>Cancel</FormButton>
         </div>
       </div>
     </div>
